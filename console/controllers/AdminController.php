dff7687d563522972f86f193ae9298fadadc1e1a
geksor <stmebelru@vh15.sweb.ru>
2018-12-25 09:45:09 +0300

    spaceWeb geksor

--- console/controllers/AdminController.php
+++ console/controllers/AdminController.php
@@ -12,7 +12,7 @@ class AdminController extends Controller
         $user = new User();
         $user->username = 'admin';
         $user->email = 'admin@example.ru';
-        $user->setPassword(123456);
+        $user->setPassword('123456789aa');
         $user->generateAuthKey();
 
         $user->save();
